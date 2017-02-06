9a4ffece04982a1310740c8f480155fe91f0e94a
eric12342006 <a0321050@gmail.com>
2017-02-07 02:34:14 +0800

    shopping cart 撞

--- shoppingcart.php
+++ shoppingcart.php
@@ -5,9 +5,20 @@
 <link rel="stylesheet" href="css/layout.css">
 </head>
 
+<?php session_start() ?>
+
 <script>
-	function del(){
+	function del(id){
+		alert(id);
+		var name = id;
 		window.location.href='delcart.php';
+	}
+	function order(id){
+		alert(id);
+		window.location.href='orderone.php';
+	}
+	function orderall(){
+		window.location.href='orderall.php';
 	}
 </script>
 <body>
@@ -27,7 +38,6 @@
 </br>
 </br>
 <?php
-session_start();
 $x = 0;
 
 if(!isset($_SESSION['user'])) {
@@ -52,25 +62,32 @@ echo "<table border='1'>
 <th>Price</th>
 <th>Quality</th>
 <th>Delete</th>
+<th>Order</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
 {
+$x ++;
 echo "<tr>";
 echo "<td><img src='".$row['image']. "' alt='' height='100' width='100' /></td> ";
-//echo "<td>" . $row['username'] . "</td>";
 $p_name = $row['p_name'];
 echo "<td>" . $row['p_name'] . "</td>";
 echo "<td>" . $row['price'] . "</td>";
 echo "<td>".$row['quantity']."</td>";
-//echo "<td><input type='button' value='Delete' onclick="header('Location:delcart.php)"></td>";
 ?>
-<td><input type='button' name ='' value='Delete' onclick='del()'></td>
+<script type="text/javascript">
+	var num = "<?php echo $x; ?>";
+	document.write("<td><input type='button' id='" + num + "' value='Delete' onclick='del(this.id)'></td>");
+	document.write("<td><input type='button' id='" + num + "'  value='Order' onclick='order(this.id)'></td>");
+</script>
 <?php
 echo "</tr>";
 }
 echo "</table>";
-
+?>
+<br/>
+<input type="button" value="Order All" onclick="orderall()">
+<?php
 mysqli_close($con);
 ?>
 
