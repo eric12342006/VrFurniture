c988735bbe53831bb0a926d79919edb89904eab8
ken420a <ken420a@hotmail.com>
2017-02-06 22:49:48 +0800

    furniture UPDATE

--- furniture.sql
+++ furniture.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2017-02-05 16:06:12
+-- 產生時間： 2017-02-06 15:49:20
 -- 伺服器版本: 10.1.10-MariaDB
 -- PHP 版本： 5.6.19
 
@@ -29,35 +29,42 @@ SET time_zone = "+00:00";
 CREATE TABLE `furniture` (
   `ID` varchar(20) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `price` double NOT NULL,
+  `price` double DEFAULT '100',
   `designer` varchar(30) NOT NULL,
   `category` varchar(20) NOT NULL,
   `desc` varchar(100) NOT NULL,
-  `color` varchar(20) NOT NULL
+  `color` varchar(20) NOT NULL,
+  `src` varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- 資料表的匯出資料 `furniture`
 --
 
-INSERT INTO `furniture` (`ID`, `name`, `price`, `designer`, `category`, `desc`, `color`) VALUES
-('bd1', 'Bed', 0, '', '', '', 'white'),
-('cab1', 'sek1', 0, '', '', '', 'wood'),
-('cab2', 'sek2', 0, '', '', '', 'wood'),
-('cab3', 'sek3', 0, '', '', '', ''),
-('cab4', 'sek4', 0, '', '', '', ''),
-('cab5', 'tumba_fur', 0, '', '', '', ''),
-('cab6', 'TV_Shell', 0, '', '', '', 'white'),
-('ch1', 'ArmChair', 1000, '', '', '', 'white'),
-('ch2', 'Cheir', 0, '', '', '', 'white'),
-('ot1', 'Lamp', 0, '', '', '', 'white'),
-('sf1', 'Sofa', 0, '', '', '', 'white'),
-('sf2', 'bed2', 0, '', '', '', 'purplish re'),
-('sf3', 'bed1', 0, '', '', '', 'blue'),
-('tb1', 'Coffe_table', 0, '', '', '', 'white'),
-('tb2', 'Table', 0, '', '', '', 'white'),
-('tb3', 'table-low-1', 0, '', '', '', 'black'),
-('tb4', 'glass_table', 0, '', '', '', '');
+INSERT INTO `furniture` (`ID`, `name`, `price`, `designer`, `category`, `desc`, `color`, `src`) VALUES
+('bd1', 'Bed', 1000, '', 'bed', '', 'white', 'img\\Gray Furniture Pack\\bed\\bed'),
+('bd2', 'bed2', 100, '', 'bed', '', 'black', ''),
+('cab1', 'sek1', 1000, '', 'cabinet', '', 'wood', ''),
+('cab2', 'sek2', 1000, '', 'cabinet', '', 'wood', ''),
+('cab3', 'sek3', 1000, '', 'cabinet', '', 'wood', ''),
+('cab4', 'sek4', 1000, '', 'cabinet', '', 'wood', ''),
+('cab5', 'tumba_fur', 1000, '', 'cabinet', '', '', ''),
+('cab6', 'TV_Shell', 1000, '', 'cabinet', '', 'white', ''),
+('ch1', 'ArmChair', 1000, '', 'chair', '', 'white', ''),
+('ch2', 'Cheir', 1000, '', 'chair', '', 'white', ''),
+('ch3', 'ch3', 100, '', '', '', '', ''),
+('ch4', 'ch2', 100, '', '', '', 'red', ''),
+('ch5', '7103', 100, '', '', '', 'black', ''),
+('ch6', '7106', 100, '', '', '', '', ''),
+('ot1', 'Lamp', 1000, '', 'other', '', 'white', ''),
+('sf1', 'Sofa', 1000, '', 'Sofa', '', 'white', ''),
+('sf2', 'bed2', 1000, '', 'Sofa', '', 'purplish re', ''),
+('sf3', 'bed1', 1000, '', 'Sofa', '', 'blue', ''),
+('sf4', 'ModernSofa', 100, '', '', '', 'white', ''),
+('tb1', 'Coffe_table', 1000, '', 'Table', '', 'white', ''),
+('tb2', 'Table', 1000, '', 'Table', '', 'white', ''),
+('tb3', 'table-low-1', 1000, '', 'Table', '', 'black', ''),
+('tb4', 'glass_table', 1000, '', 'Table', '', '', '');
 
 --
 -- 已匯出資料表的索引
